77b853f2f9ae0f3f3422353a21996dc8d2f1675e
LEO1234567890LEO <95534988+LEO1234567890LEO@users.noreply.github.com>
2023-12-06 23:12:30 +0530

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+BOT_TOKEN="5633958373:AAEwNftPi7R59T7d-20cpUMUholsf3olN7U"
+USER_ID="5339665987"
+
+
+MESSAGE="hi its began"
+
+API_ENDPOINT="https://api.example.com/send-message"
+
+curl -X POST -H "Authorization: Bearer $BOT_TOKEN" -H "Content-Type: application/json" -d "{\"user_id\": \"$USER_ID\", \"message\": \"$MESSAGE\"}" $API_ENDPOINT
